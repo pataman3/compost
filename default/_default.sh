5b14ad52525a3d547361dc30ec3f1c31e54a9fd2
Bryan Bean <19828900+pataman3@users.noreply.github.com>
2026-01-19 22:50:00 -0500

    Revised brutish _default.sh

--- default/_default.sh
+++ default/_default.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-docker compose -f audiobookshelf.yml --env-file .env.audiobookshelf up -d --force-recreate
-docker compose -f filebrowser.yml --env-file .env.filebrowser up -d --force-recreate
-docker compose -f homarr.yml --env-file .env.homarr up -d --force-recreate
-docker compose -f jellyfin.yml --env-file .env.jellyfin up -d --force-recreate
-docker compose -f jellyseerr.yml --env-file .env.jellyseerr up -d --force-recreate
-docker compose -f profilarr.yml --env-file .env.profilarr up -d --force-recreate
-docker compose -f silverbullet.yml --env-file .env.silverbullet up -d --force-recreate
-docker compose -f suwayomi.yml --env-file .env.suwayomi up -d --force-recreate
-docker compose -f uptime-kuma.yml --env-file .env.uptime-kuma up -d --force-recreate
-docker compose -f vikunja.yml --env-file .env.vikunja up -d --force-recreate
-docker compose -f watchtower.yml --env-file .env.watchtower up -d --force-recreate
+services=(
+  audiobookshelf
+  filebrowser
+  homarr
+  jellyfin
+  jellyseerr
+  profilarr
+  silverbullet
+  suwayomi
+  uptime-kuma
+  vikunja
+  watchtower
+)
+
+for service in "${services[@]}"; do
+  docker compose -f "${service}.yml" --env-file ".env.${service}" up -d --force-recreate
+done
